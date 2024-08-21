713428701a9719779a9ad7be5961e998bb6794ac
Anthony Yung <ycya88@gmail.com>
2024-08-22 04:31:00 +0800

    Update sha256

--- lddc.rb
+++ lddc.rb
@@ -5,10 +5,10 @@ class Lddc < Formula
   
     if Hardware::CPU.intel?
       url "https://github.com/opslabhqx/homebrew-lddc/releases/download/v1.0.0/lddc-v1.0.0-linux-amd64.tar.gz"
-      sha256 "0dba2f23a4ca8d1274066317fffc89029111f6cd3a3a65e72b56e73c57ab2560"
+      sha256 "c2413e3e8c6246220196569ee5bec4092706536d722597ca8e82978f860272eb"
     elsif Hardware::CPU.arm?
       url "https://github.com/opslabhqx/homebrew-lddc/releases/download/v1.0.0/lddc-v1.0.0-linux-arm64.tar.gz"
-      sha256 "8dd96f02b5f2522defcf91bc67693c7736894091c9688387acec8b1b8506f069"
+      sha256 "6d425c04b5cc92cf2edaaa5cdd8169776c724249b10014ab453cb1f8af77e50d"
     end
   
     license "MIT"
