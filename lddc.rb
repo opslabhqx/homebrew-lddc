86c9ebb45eca6dc5340925dad5e9bc4fef13a835
Anthony Yung <yhs88a@gmail.com>
2024-09-27 16:16:55 +0000

    Update Homebrew formula for version v1.11.0

--- lddc.rb
+++ lddc.rb
@@ -1,18 +1,18 @@
 class Lddc < Formula
-    desc "A simple script that collects and copies shared library dependencies of a specified binary to a given directory. "
+    desc "A simple script that collects and copies shared library dependencies of a specified binary to a given directory."
     homepage "https://github.com/opslabhqx/lddc"
-    version "1.0.0"
-  
+    version "#v"
+
     if Hardware::CPU.intel?
-      url "https://github.com/opslabhqx/lddc/releases/download/v1.0.0/lddc-v1.0.0-linux-amd64.tar.gz"
-      sha256 "c2413e3e8c6246220196569ee5bec4092706536d722597ca8e82978f860272eb"
+      url "https://github.com/opslabhqx/lddc/releases/download//lddc--linux-amd64.tar.gz"
+      sha256 ""
     elsif Hardware::CPU.arm?
-      url "https://github.com/opslabhqx/lddc/releases/download/v1.0.0/lddc-v1.0.0-linux-arm64.tar.gz"
-      sha256 "6d425c04b5cc92cf2edaaa5cdd8169776c724249b10014ab453cb1f8af77e50d"
+      url "https://github.com/opslabhqx/lddc/releases/download//lddc--linux-arm64.tar.gz"
+      sha256 ""
     end
-  
+
     license "MIT"
-  
+
     def install
       bin.install "lddc"
     end
